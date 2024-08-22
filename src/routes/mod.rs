145cfea4881652ab09a0968469e68d05155fb140
mattboan <matt@voidstudios.com.au>
2024-08-22 20:42:55 +0800

    removed warnings

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,17 +7,8 @@ use crate::AppState;
 use crate::routes::form::init_form_routes;
 use crate::routes::user::init_user_routes;
 use axum::{
-    body::Body,
-    http::{Request, Response},
-    middleware::Next,
-    response::IntoResponse,
     Router,
 };
-use std::convert::Infallible;
-use std::future::Future;
-
-
-
 
 pub fn init_routes(state: Arc<AppState>) -> Router {
     return Router::new()
